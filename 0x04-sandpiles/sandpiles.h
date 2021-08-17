4212b006541bcb969d3552233d6ba1b9abcaa2fa
Nicolas <n.ico15935746@gmail.com>
2021-08-17 18:19:31 -0500

    [Added] Prototypes for sandpile functions

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void check_sandpile(int grid[3][3]);
+int is_perfect_sand(int grid[3][3]);
+
+#endif /* SANDPILES_H */
