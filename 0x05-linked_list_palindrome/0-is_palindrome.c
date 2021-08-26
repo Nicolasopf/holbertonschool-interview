8dbad2448db3af6093e812a49f247f6ca5cf695a
Nicolas <n.ico15935746@gmail.com>
2021-08-26 01:00:57 -0500

    [Added] Check if a linked list is a palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - Check if a linked list is a palindrome.
+ * @head: pointer to head node.
+ * Return: 1 if palindrome, 0 if not.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int nums[1024], index, j;
+	listint_t *tmp = *head;
+
+	if (!*head)
+		return (1);
+
+	for (index = 0; tmp; index++)
+	{
+		nums[index] = tmp->n;
+		tmp = tmp->next;
+	}
+
+	for (j = 0; j < index; j++)
+		if (nums[j] != nums[index - j - 1])
+			return (0);
+
+	return (1);
+}
